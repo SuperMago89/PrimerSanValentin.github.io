c165b483cd250dc05ea0808243e7990dffa56e3f
SuperMago89 <107944474+SuperMago89@users.noreply.github.com>
2024-02-07 16:52:37 -0500

    InitialCommit

---
+++ index.js
@@ -0,0 +1,21 @@
+const yesBtn = document.querySelector('#yesBtn');
+const gifOverlay = document.getElementById('gifOverlay');
+const gifChange = document.getElementById('gifChange');
+
+yesBtn.addEventListener('click',function () {
+    alert('Te amo muchísimo mi vida hermosa hermosa 💞')
+    const contenedorImg = document.getElementById('contenedorImg');
+    contenedorImg.style.justifyContent = 'center';
+    gifOverlay.style.display = 'block'; // Mostrar la capa de superposición con el gif
+    gifChange.innerHTML = '<img src="/Poto.gif" alt="" style="height: 300px;width: 533.34px;">'; // Cambiar el gif debajo de los botones
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
